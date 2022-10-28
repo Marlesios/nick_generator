b7c913d82e38610b70aa9516a1a769a4a0a7d92f
Marlesios <marles.nuralyev@gmail.com>
2022-10-28 17:11:43 +0300

    version 1.2

--- src/main/java/com/example/atomics/Count.java
+++ src/main/java/com/example/atomics/Count.java
@@ -6,11 +6,14 @@ import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
+
 public class Count {
     AtomicInteger count = new AtomicInteger(0);
-    //    int sampleInt;
+    AtomicInteger equalName = new AtomicInteger(0);
     String sample;
     List<String> five = new ArrayList<>();
+    List<String> sameNames = new ArrayList<>();
 
     public void setCount(String[] text, int length) {
         for (String s : text) {
@@ -33,6 +36,29 @@ public class Count {
         System.out.printf("красивых слов состоящих из %s в количестве %s \n", length, count);
 
 
+    }
+    public void sameName(String[] text) {
+        for (String s : text) {
+            char[] current = s.toCharArray();
+            int count = 0;
+                for(int i= 0; i < current.length; i++){
+                    for(int j = i + 1; j < current.length; j++){
+                        char one = current[j];
+                        if(current[i] == one){
+                            count += 1;
+                        }
+                    }
+                }
+                if(current.length  == count){
+                    equalName.addAndGet(1);
+                    sameNames.add(s);
+                }
+        }
+
+        System.out.println("количество имен с одинаковыми буквами : "+equalName);
+        for(String s: sameNames){
+            System.out.println(s);
+        }
     }
 
     public Integer getCount() {
